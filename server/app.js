750486d0c3de634de171860e0440b8ae4224db1b
noavrd <noavrd@gmail.com>
2022-01-01 00:58:35 +0200

    get information on pull requests

--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+const { Octokit } = require('@octokit/core');
+
 const app = express();
+const octokit = new Octokit({ auth: process.env.TOKEN });
 
 app.use(express.json());
 app.use(cors());
@@ -10,6 +13,20 @@ app.get('/', (req, res) => {
     res.status(200).send('Main GET');
   } catch {
     res.status(500).send('Server problem');
+  }
+});
+
+app.get('/repo', async (req, res) => {
+  try {
+    //change repo
+    const response = await octokit.request('GET /repos/{owner}/{repo}/pulls', {
+      owner: 'noavrd',
+      repo: 'url-shortner',
+    });
+
+    res.send(response);
+  } catch (err) {
+    res.status(500).send(err);
   }
 });
 
